8d6e2e350dd2a6d7e3ac4aad753ab8d6cf80f468
Edgar Castillo Vega <vayne.edgar@gmail.com>
2023-06-08 22:59:52 -0600

    fix: some issues were fixed on the package.json file on root project [v1]

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+const webpack = require("webpack");
+const { parsed: myEnv } = require("dotenv").config({});
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
+  },
+  swcMinify: true,
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    return config;
   },
   exportPathMap() {
     return {
